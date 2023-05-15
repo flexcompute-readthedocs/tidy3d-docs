b3a24deaaacd694ce8c645bf5becf4cd702c5a1c
lius-magic <liushuang@magicloud.io>
2023-05-15 23:04:22 +0800

    feat: customize meta information for rst file

--- docs/source/_ext/custom_meta.py
+++ docs/source/_ext/custom_meta.py
@@ -1,6 +1,7 @@
 from sphinx.util import logging
 from nbconvert import HTMLExporter
 import nbformat
+from bs4 import BeautifulSoup
 import os
 import sys
 import re
@@ -14,30 +15,35 @@ def html_page_context(app, pagename, templatename, context, doctree):
     with the value of the 'html_title' configuration option.
     '''
     logger.debug('Updating HTML page context')
-    print("HELLO-------------------->")
     notebook_path = app.env.doc2path(os.path.abspath("source/" +pagename), base=None)
-    if not notebook_path.endswith('.ipynb'):
-        return
-    # print(list(context.keys()))
-    with open(notebook_path, 'r', encoding='utf-8') as f:
-        nb = nbformat.read(f, as_version=4)
-        html_exporter = HTMLExporter()
-        (html_body, resources) = html_exporter.from_notebook_node(nb)
-        # Modify the HTML title tag to use the notebook's metadata
-        metadata = nb['metadata']
-        title = metadata.get('title', '')
-        description = metadata.get('description', '')
-        keywords = metadata.get('keywords', '')
-        print(title,description,keywords)
-        meta_list = []
-        if title:
-            context['title'] = title
-        if keywords:
-            meta_list.append(f'\n<meta itemprop="keywords" content="{keywords}" />')
-        if description:
-            meta_list.append(f'\n<meta name="description" content="{description}" />')
+    if notebook_path.endswith('.ipynb'):
+        # print(list(context.keys()))
+        with open(notebook_path, 'r', encoding='utf-8') as f:
+            nb = nbformat.read(f, as_version=4)
+            html_exporter = HTMLExporter()
+            (html_body, resources) = html_exporter.from_notebook_node(nb)
+            # Modify the HTML title tag to use the notebook's metadata
+            metadata = nb['metadata']
+            title = metadata.get('title', '')
+            description = metadata.get('description', '')
+            keywords = metadata.get('keywords', '')
+            print(title,description,keywords)
+            meta_list = []
+            if title:
+                context['title'] = title
+            if keywords:
+                meta_list.append(f'\n<meta itemprop="keywords" content="{keywords}" />')
+            if description:
+                meta_list.append(f'\n<meta name="description" content="{description}" />')
 
-        context['metatags'] = "\n".join(meta_list)
+            context['metatags'] = "\n".join(meta_list)
+    if notebook_path.endswith('.rst') and hasattr(doctree, 'traverse'):
+        for node in doctree.traverse():
+            if node.tagname == "meta":
+                soup = BeautifulSoup(node.pformat(), 'html.parser')
+                meta = soup.find('meta')
+                if meta.get('name') == 'html_seo_title':
+                    context['title'] = meta.get('content')
 
 def setup(app):
     app.connect('html-page-context', html_page_context)
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,6 +1,11 @@
 Tidy3D Documentation
 ====================
 
+.. meta::
+   :description: The reStructuredText plaintext markup language
+   :keywords: plaintext, markup language, Customize, yyy, xx
+   :html_seo_title: Custom Page Title ----
+
 Tidy3D is a software package for solving extremely large electrodynamics problems using the finite-difference time-domain (FDTD) method.
 
 This is the documentation for the python API, which is the main way to build simulation models, submit jobs, and analyze results of completed runs.
