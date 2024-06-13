55c0fe98eae9bd406379ca5894513f62ddfb11e8
momchil-flex <92756559+momchil-flex@users.noreply.github.com>
2024-06-13 18:47:58 +0000

    Deploying to develop from @ flexcompute/tidy3d@129f5af2d6b8a08e421000e4366fca4400346f4d 🚀

--- tidy3d/components/autograd.py
+++ tidy3d/components/autograd.py
@@ -1,5 +1,6 @@
 # utilities for working with autograd
 
+import copy
 import typing
 
 import numpy as np
@@ -12,9 +13,18 @@ from tidy3d.components.type_util import _add_schema
 
 from .types import ArrayFloat2D, ArrayLike, Bound, Size1D
 
+# add schema to the Box
 _add_schema(Box, title="AutogradBox", field_type_str="autograd.tracer.Box")
 
-# TODO: should we use ArrayBox? Box is more general
+# make sure Boxes in tidy3d properly define VJPs for copy operations, for computational graph
+_copy = primitive(copy.copy)
+_deepcopy = primitive(copy.deepcopy)
+
+defvjp(_copy, lambda ans, x: lambda g: _copy(g))
+defvjp(_deepcopy, lambda ans, x, memo: lambda g: _deepcopy(g, memo))
+
+Box.__copy__ = lambda v: _copy(v)
+Box.__deepcopy__ = lambda v, memo: _deepcopy(v, memo)
 
 # Types for floats, or collections of floats that can also be autograd tracers
 TracedFloat = typing.Union[float, Box]
